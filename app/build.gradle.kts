5565e910ef93c6ef76e8b515de99cfc3b704bf38
Bulkawitch <Bulkawitch@users.noreply.github.com>
2026-09-23 18:21:46 +0300

    Release 0.1.2: bump version to 0.1.2, recalibrated floor 4 map grid, dynamic header version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ru.nya.nyeios"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.1.1"
+        versionCode = 8
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
